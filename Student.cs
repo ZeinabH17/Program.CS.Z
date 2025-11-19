cecc8f8319f72518c6d86f318561ffc215ab5e3b
ZeinabH17 <zeinabhussein105@gmail.com>
2025-11-18 20:00:30 -0500

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CodeFirstDemo
+{
+    public class Student
+    {
+        public int StudentId { get; set; }  // Primary key
+        public string StudentName { get; set; }
+  
